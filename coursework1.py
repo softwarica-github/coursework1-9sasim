d4a66f955f0e3e1a12fe0426d510cd75de83b96c
Sudat Sasim Shrestha <sudatsasimshrestha@gmail.com>
2023-06-02 17:26:53 +0545

    steganography tool

---
+++ coursework1.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import filedialog
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
+from stegano import lsb #pip install stegeano
+root = Tk()
+root.title("helo")
+root.geometry("700x500")    
+
+
+
+root.mainloop()
