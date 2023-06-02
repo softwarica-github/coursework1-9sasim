b8004a6439a66e84df94c2d3326966734826a582
9sasim <105635477+9sasim@users.noreply.github.com>
2023-06-02 17:28:29 +0545

    Update coursework1.py

--- coursework1.py
+++ coursework1.py
@@ -3,9 +3,9 @@ from tkinter import filedialog
 import tkinter as tk
 from PIL import Image, ImageTk
 import os
-from stegano import lsb #pip install stegeano
+from stegano import lsb #pip install stegano
 root = Tk()
-root.title("helo")
+root.title("Steganography Tool")
 root.geometry("700x500")    
 
 
